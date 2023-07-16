f3849c39796876d1a7a3deba24bae9c49310da0e
roman-snowflake <139580913+roman-snowflake@users.noreply.github.com>
2023-07-16 13:43:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,5 @@ my_data_rows = my_cur.fetchall()
 
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-streamlit.text("What would you like to add?")
+
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_data_rows.index))
