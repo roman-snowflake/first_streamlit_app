fe381bd9dbbb6ff0b453b965782d1d9d0a1a7633
roman-snowflake <139580913+roman-snowflake@users.noreply.github.com>
2023-07-16 14:40:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,14 +48,15 @@ def get_fruit_load_list():
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
-if streamlit.button('Get Fruit Load List'):  
+if streamlit.button('Get Fruit List'):  
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values (add_my_fruit)")
+    my_cur.execute("insert into fruit_load_list values ("+ add_my_fruit + ")")
     return streamlit.write('Thanks for adding ', add_my_fruit)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
